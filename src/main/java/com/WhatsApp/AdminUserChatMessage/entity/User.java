4ef47ad17e32a534f4a2eaea20cf4cfa27dd1907
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-09 20:20:48 +0530

    User.java Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/entity/User.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/entity/User.java
@@ -1,4 +1,82 @@
 package com.WhatsApp.AdminUserChatMessage.entity;
 
-public class User {
+import com.WhatsApp.AdminUserChatMessage.entity.enums.Role;
+import com.WhatsApp.AdminUserChatMessage.entity.enums.UserStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<PresentEvent> presentEvents;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<ChatMessage> chatMessages;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
