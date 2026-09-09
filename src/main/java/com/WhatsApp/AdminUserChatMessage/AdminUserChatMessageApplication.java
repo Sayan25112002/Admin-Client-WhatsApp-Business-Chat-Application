4e4ffd47ba5b9ce8f285709048f1bb51ab0d4fe0
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-09 18:07:17 +0530

    First Project Setup

---
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/AdminUserChatMessageApplication.java
@@ -0,0 +1,13 @@
+package com.WhatsApp.AdminUserChatMessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminUserChatMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminUserChatMessageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WhatsApp/AdminUserChatMessage/AdminUserChatMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WhatsApp.AdminUserChatMessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminUserChatMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
