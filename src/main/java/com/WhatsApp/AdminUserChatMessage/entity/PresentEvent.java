12f2ca500ba7a89b9e7018b323471ecd6e107990
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-09 20:20:22 +0530

    PresentEvent.java Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/entity/PresentEvent.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/entity/PresentEvent.java
@@ -1,4 +1,31 @@
 package com.WhatsApp.AdminUserChatMessage.entity;
 
+import com.WhatsApp.AdminUserChatMessage.entity.enums.MessageType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 public class PresentEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    @JsonIgnore
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private MessageType messageType;
+
+    private LocalDateTime dateTime;
+
 }
