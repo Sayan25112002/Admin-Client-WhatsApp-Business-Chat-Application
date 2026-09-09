156775848ba0bf713025a5d47f6c46ac314041e3
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-09 20:18:54 +0530

    BroadCastMessage.java Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/entity/BroadCastMessage.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/entity/BroadCastMessage.java
@@ -1,4 +1,32 @@
 package com.WhatsApp.AdminUserChatMessage.entity;
 
+import com.WhatsApp.AdminUserChatMessage.entity.enums.MessageType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
 public class BroadCastMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="userId")
+    @JsonIgnore
+    private User user;
+
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private MessageType messageType;
+
+    private LocalDateTime dateTime;
+
 }
