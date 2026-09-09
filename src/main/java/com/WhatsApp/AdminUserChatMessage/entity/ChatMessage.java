feaf8978090936426a28e2640886ce52cc0718d2
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-09 20:19:21 +0530

    ChatMessage.java Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/entity/ChatMessage.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/entity/ChatMessage.java
@@ -1,4 +1,37 @@
 package com.WhatsApp.AdminUserChatMessage.entity;
 
+import com.WhatsApp.AdminUserChatMessage.entity.enums.MessageType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
 public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "conversationId")
+    @JsonIgnore
+    private Conversation conversation;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    @JsonIgnore
+    private User user;
+
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private MessageType messageType;
+
+    private LocalDateTime createdAt;
+
 }
