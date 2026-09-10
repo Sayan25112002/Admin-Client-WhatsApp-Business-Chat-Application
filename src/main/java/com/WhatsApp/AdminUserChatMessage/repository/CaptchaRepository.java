6475ed7a307c55fa09d5b42008823be5fd47af83
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-10 16:03:24 +0530

    CaptchaRepository Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/repository/CaptchaRepository.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/repository/CaptchaRepository.java
@@ -1,4 +1,9 @@
 package com.WhatsApp.AdminUserChatMessage.repository;
 
-public interface CaptchaRepository {
+import com.WhatsApp.AdminUserChatMessage.entity.Captcha;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CaptchaRepository extends JpaRepository<Captcha,Long> {
 }
