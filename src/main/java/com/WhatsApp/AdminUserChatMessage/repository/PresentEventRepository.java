5192a3d141006ffda2cd9771fe38fa11211456e6
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-10 16:09:54 +0530

    PresentEventRepository Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/repository/PresentEventRepository.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/repository/PresentEventRepository.java
@@ -1,4 +1,9 @@
 package com.WhatsApp.AdminUserChatMessage.repository;
 
-public interface PresentEventRepository {
+import com.WhatsApp.AdminUserChatMessage.entity.PresentEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PresentEventRepository extends JpaRepository<PresentEvent,Long> {
 }
