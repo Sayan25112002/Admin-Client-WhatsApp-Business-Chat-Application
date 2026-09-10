ced0877c0843ac2157daf26055fc8f7d013980f4
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-10 11:55:17 +0530

    LoginRequestDto.java Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/dto/requestDto/LoginRequestDto.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/dto/requestDto/LoginRequestDto.java
@@ -1,4 +1,22 @@
 package com.WhatsApp.AdminUserChatMessage.dto.requestDto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LoginRequestDto {
+
+    private String email;
+
+    private String password;
+
+    private String captchaId;
+
+    private String captchaAnswer;
+
 }
