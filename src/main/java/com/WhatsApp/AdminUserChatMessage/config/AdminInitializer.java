3b640308e56e27fbbc4215bf951345d0d0291250
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-10 16:42:40 +0530

    AdminInitializer.java Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/config/AdminInitializer.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/config/AdminInitializer.java
@@ -1,4 +1,37 @@
 package com.WhatsApp.AdminUserChatMessage.config;
 
+import com.WhatsApp.AdminUserChatMessage.entity.User;
+import com.WhatsApp.AdminUserChatMessage.entity.type.Role;
+import com.WhatsApp.AdminUserChatMessage.entity.type.UserStatus;
+import com.WhatsApp.AdminUserChatMessage.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.mapstruct.control.MappingControl;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
 public class AdminInitializer {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void init() {
+        if(userRepository.count()==0){
+            User user = User.builder()
+                    .name("ADMIN")
+                    .contact("9876543210")
+                    .email("admin123@gmail.com")
+                    .password(passwordEncoder.encode("Admin@123"))
+                    .role(Role.ADMIN)
+                    .status(UserStatus.ONLINE)
+                    .createdAt(LocalDateTime.now())
+                    .build();
+            userRepository.save(user);
+        }
+    }
 }
