4121a881c80d1a05122a60e28c0c5a306d7502e5
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-10 16:31:43 +0530

    PresentEventMapper.java Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/mapper/PresentEventMapper.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/mapper/PresentEventMapper.java
@@ -1,4 +1,16 @@
 package com.WhatsApp.AdminUserChatMessage.mapper;
 
+import com.WhatsApp.AdminUserChatMessage.dto.responseDto.PresentEventResponseDto;
+import com.WhatsApp.AdminUserChatMessage.entity.PresentEvent;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface PresentEventMapper {
+
+    PresentEventResponseDto toPresentEventResponseDto(PresentEvent presentEvent);
+
+    List<PresentEventResponseDto> toPresentEventResponseDtoList(List<PresentEvent> presentEvents);
+
 }
