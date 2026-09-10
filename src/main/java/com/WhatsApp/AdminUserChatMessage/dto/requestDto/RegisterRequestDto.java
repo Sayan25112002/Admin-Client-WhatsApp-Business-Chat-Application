0bf6b9b4e70e18a8da5f62ea80f785fba5e33dc3
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-10 11:55:45 +0530

    RegisterRequestDto.java Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/dto/requestDto/RegisterRequestDto.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/dto/requestDto/RegisterRequestDto.java
@@ -1,4 +1,26 @@
 package com.WhatsApp.AdminUserChatMessage.dto.requestDto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RegisterRequestDto {
+
+    private String name;
+
+    private String contact;
+
+    private String email;
+
+    private String password;
+
+    private String captchaId;
+
+    private String captchaAnswer;
+
 }
