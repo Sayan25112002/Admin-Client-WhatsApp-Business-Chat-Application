75ecb5119f55b09b809a1735cc04f1270bbbcbbe
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-10 18:06:09 +0530

    AuthUtil.java Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/security/AuthUtil.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/security/AuthUtil.java
@@ -1,4 +1,47 @@
 package com.WhatsApp.AdminUserChatMessage.security;
 
+import com.WhatsApp.AdminUserChatMessage.entity.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
 public class AuthUtil {
+
+    @Value("${jwt.secretKey}")
+    private String jwtSecretKey;
+
+    private SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(jwtSecretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateAccessToken(User user) {
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .claim("tokenType","ACCESS")
+                .claim("userId",user.getId().toString())
+                .claim("role",user.getRole().toString())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000l*60*20))
+                .signWith(getSecretKey())
+                .compact();
+    }
+
+    public String generateRefreshToken(User user) {
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .claim("tokenType","REFRESH")
+                .claim("userId",user.getId().toString())
+                .claim("role",user.getRole().toString())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis()+1000L*60*60*24*7))
+                .signWith(getSecretKey())
+                .compact();
+    }
+
 }
