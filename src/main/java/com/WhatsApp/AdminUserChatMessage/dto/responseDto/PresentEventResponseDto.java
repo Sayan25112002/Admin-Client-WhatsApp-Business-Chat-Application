a33b680e4ee2f7812342e84c6d9c5ff5993de0e9
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-10 13:04:04 +0530

    PresentEventResponseDto.java Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/dto/responseDto/PresentEventResponseDto.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/dto/responseDto/PresentEventResponseDto.java
@@ -1,4 +1,25 @@
 package com.WhatsApp.AdminUserChatMessage.dto.responseDto;
 
+import com.WhatsApp.AdminUserChatMessage.entity.type.MessageType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class PresentEventResponseDto {
+
+    private Long id;
+
+    private Long userId;
+
+    private MessageType messageType;
+
+    private LocalDateTime createdAt;
+
 }
