a5403c7972db9991123d9f42319199177770361b
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-10 16:30:42 +0530

    ChatMessageMapper.java Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/mapper/ChatMessageMapper.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/mapper/ChatMessageMapper.java
@@ -1,4 +1,19 @@
 package com.WhatsApp.AdminUserChatMessage.mapper;
 
+import com.WhatsApp.AdminUserChatMessage.dto.requestDto.ChatMessageRequestDto;
+import com.WhatsApp.AdminUserChatMessage.dto.responseDto.ChatMessageResponseDto;
+import com.WhatsApp.AdminUserChatMessage.entity.ChatMessage;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface ChatMessageMapper {
+
+    ChatMessage toChatMessageDto(ChatMessageRequestDto chatMessageRequestDto);
+
+    ChatMessageResponseDto toChatMessageResponseDto(ChatMessage chatMessage);
+
+    List<ChatMessageResponseDto> toChatMessageResponseDtoList(List<ChatMessage> chatMessages);
+
 }
