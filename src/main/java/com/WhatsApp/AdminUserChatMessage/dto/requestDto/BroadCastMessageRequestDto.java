1838246c3975026eb63c1dacf633fb8cbe17f5c6
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-10 11:53:36 +0530

    BroadCastMessageRequestDto.java Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/dto/requestDto/BroadCastMessageRequestDto.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/dto/requestDto/BroadCastMessageRequestDto.java
@@ -1,4 +1,16 @@
 package com.WhatsApp.AdminUserChatMessage.dto.requestDto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class BroadCastMessageRequestDto {
+
+    private String content;
+
 }
