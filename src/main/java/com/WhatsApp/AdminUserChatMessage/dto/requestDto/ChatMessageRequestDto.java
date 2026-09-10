aa1eca61646d9df3fbd73dbfbd33295d2f20be0b
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-10 11:54:51 +0530

    ChatMessageRequestDto.java Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/dto/requestDto/ChatMessageRequestDto.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/dto/requestDto/ChatMessageRequestDto.java
@@ -1,4 +1,18 @@
 package com.WhatsApp.AdminUserChatMessage.dto.requestDto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ChatMessageRequestDto {
+
+    private Long conversationId;
+
+    private String content;
+
 }
