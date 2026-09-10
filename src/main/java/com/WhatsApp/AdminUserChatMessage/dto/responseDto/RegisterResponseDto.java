8bf78c1eb21f742576790d5ba445e211abcdd79d
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-10 13:05:06 +0530

    RegisterResponseDto.java Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/dto/responseDto/RegisterResponseDto.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/dto/responseDto/RegisterResponseDto.java
@@ -1,4 +1,18 @@
 package com.WhatsApp.AdminUserChatMessage.dto.responseDto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RegisterResponseDto {
+
+    private String message;
+
+    private String verificationId;
+
 }
