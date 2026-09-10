5641c736a616ed42895517887d36bfde4eb5adce
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-10 16:11:12 +0530

    UserRepository Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/repository/UserRepository.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.WhatsApp.AdminUserChatMessage.repository;
 
-public interface UserRepository {
+import com.WhatsApp.AdminUserChatMessage.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
 }
