328644238b06e138e472e2099ac18a2f3f22826d
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-10 13:01:47 +0530

    BroadCastMessageResponseDto.java Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/dto/responseDto/BroadCastMessageResponseDto.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/dto/responseDto/BroadCastMessageResponseDto.java
@@ -1,4 +1,27 @@
 package com.WhatsApp.AdminUserChatMessage.dto.responseDto;
 
+import com.WhatsApp.AdminUserChatMessage.entity.type.MessageType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class BroadCastMessageResponseDto {
+
+    private Long id;
+
+    private Long senderId;
+
+    private String content;
+
+    private MessageType messageType;
+
+    private LocalDateTime createdAt;
+
 }
