c3cbcb1fa49cbb5518bcc894061affabff8c2c78
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-10 16:06:38 +0530

    OtpVerificationRepository Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/repository/OtpVerificationRepository.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/repository/OtpVerificationRepository.java
@@ -1,4 +1,9 @@
 package com.WhatsApp.AdminUserChatMessage.repository;
 
-public interface OtpVerificationRepository {
+import com.WhatsApp.AdminUserChatMessage.entity.OtpVerification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OtpVerificationRepository extends JpaRepository<OtpVerification,Long> {
 }
