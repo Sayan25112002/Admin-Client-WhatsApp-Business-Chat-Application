ad4abbbd80ef497603ddff92e4a687aad092889c
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-10 11:56:20 +0530

    VerifyOtpRequestDto.java Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/dto/requestDto/VerifyOtpRequestDto.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/dto/requestDto/VerifyOtpRequestDto.java
@@ -1,4 +1,20 @@
 package com.WhatsApp.AdminUserChatMessage.dto.requestDto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class VerifyOtpRequestDto {
+
+    private String verificationId;
+
+    private String contactOtp;
+
+    private String emailOtp;
+
 }
