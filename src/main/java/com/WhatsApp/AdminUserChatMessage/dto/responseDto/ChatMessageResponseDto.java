7602eb468e068575c4d1776096dc9d1a43d775bc
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-10 13:02:15 +0530

    ChatMessageResponseDto.java Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/dto/responseDto/ChatMessageResponseDto.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/dto/responseDto/ChatMessageResponseDto.java
@@ -1,4 +1,29 @@
 package com.WhatsApp.AdminUserChatMessage.dto.responseDto;
 
+import com.WhatsApp.AdminUserChatMessage.entity.type.MessageType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ChatMessageResponseDto {
+
+    private Long id;
+
+    private Long conversationId;
+
+    private Long senderId;
+
+    private String content;
+
+    private MessageType messageType;
+
+    private LocalDateTime createdAt;
+
 }
