2a92d4562804e39eeef1908a66f66f49052825ab
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-23 18:04:50 +0530

    CaptchaResponseDto.java Added

--- src/main/java/com/WhatsApp/AdminUserChatMessage/dto/responseDto/CaptchaResponseDto.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/dto/responseDto/CaptchaResponseDto.java
@@ -1,4 +1,20 @@
 package com.WhatsApp.AdminUserChatMessage.dto.responseDto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CaptchaResponseDto {
+
+    private String captchaId;
+
+    private String captchaAnswer;
+
+    private String captchaImage;
+
 }
