84b7115730d7120bb51563c972cc275a609f3944
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-23 19:52:33 +0530

    OTPServiceImpl Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/service/implementation/OTPServiceImpl.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/service/implementation/OTPServiceImpl.java
@@ -1,4 +1,23 @@
 package com.WhatsApp.AdminUserChatMessage.service.implementation;
 
-public class OTPServiceImpl {
+import com.WhatsApp.AdminUserChatMessage.repository.OtpVerificationRepository;
+import com.WhatsApp.AdminUserChatMessage.service.OTPService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OTPServiceImpl implements OTPService {
+
+    private final OtpVerificationRepository otpVerificationRepository;
+
+    @Override
+    public String generateOtp(String userName) {
+        return "";
+    }
+
+    @Override
+    public String validateOtpAndGetUserName(String preAuthToken, String otp) {
+        return "";
+    }
 }
