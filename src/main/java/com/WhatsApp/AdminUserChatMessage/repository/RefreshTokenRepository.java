bea7dc3a3208c5bf3cd5ea470ce1fde6ab12b562
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-24 16:14:32 +0530

    RefreshTokenRepository.java Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/repository/RefreshTokenRepository.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/repository/RefreshTokenRepository.java
@@ -1,9 +1,16 @@
 package com.WhatsApp.AdminUserChatMessage.repository;
 
 import com.WhatsApp.AdminUserChatMessage.entity.RefreshToken;
+import com.WhatsApp.AdminUserChatMessage.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Long> {
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+
+    List<RefreshToken> findAllByUser(User user);
 }
