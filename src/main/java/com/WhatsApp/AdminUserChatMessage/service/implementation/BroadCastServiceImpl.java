8fcda9a950eb686cb8293a89bb16abe3f4ff1471
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-25 15:32:04 +0530

    BroadCastServiceImpl.java Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/service/implementation/BroadCastServiceImpl.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/service/implementation/BroadCastServiceImpl.java
@@ -1,4 +1,40 @@
 package com.WhatsApp.AdminUserChatMessage.service.implementation;
 
-public class BroadCastServiceImpl {
+import com.WhatsApp.AdminUserChatMessage.dto.requestDto.BroadCastMessageRequestDto;
+import com.WhatsApp.AdminUserChatMessage.dto.responseDto.BroadCastMessageResponseDto;
+import com.WhatsApp.AdminUserChatMessage.entity.BroadCastMessage;
+import com.WhatsApp.AdminUserChatMessage.entity.User;
+import com.WhatsApp.AdminUserChatMessage.entity.type.MessageType;
+import com.WhatsApp.AdminUserChatMessage.mapper.BroadCastMapper;
+import com.WhatsApp.AdminUserChatMessage.repository.BroadCastRepository;
+import com.WhatsApp.AdminUserChatMessage.repository.UserRepository;
+import com.WhatsApp.AdminUserChatMessage.service.BroadCastService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.AccessDeniedException;
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class BroadCastServiceImpl implements BroadCastService {
+
+    private final BroadCastRepository broadCastRepository;
+    private final BroadCastMapper broadCastMapper;
+    private final UserRepository userRepository;
+
+    @Override
+    public BroadCastMessageResponseDto sendMessage(BroadCastMessageRequestDto broadCastMessageRequestDto, Long senderId) throws AccessDeniedException {
+        User sender = userRepository.findById(senderId).orElseThrow(()-> new RuntimeException("User not found"));
+        if(senderId!=1){
+            throw new AccessDeniedException("Only Admin Can send message");
+        }
+        BroadCastMessage broadCastMessage = broadCastMapper.toBroadCastMessage(broadCastMessageRequestDto);
+        broadCastMessage.setUser(sender);
+        broadCastMessage.setMessageType(MessageType.CHAT);
+        broadCastMessage.setCreatedAt(LocalDateTime.now());
+        broadCastRepository.save(broadCastMessage);
+        return broadCastMapper.toBroadCastMessageResponseDto(broadCastMessage);
+    }
+
 }
