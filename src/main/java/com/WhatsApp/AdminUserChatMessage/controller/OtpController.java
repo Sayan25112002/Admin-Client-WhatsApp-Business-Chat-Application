13d77ca26d3886c4ab600b6d075818fda0c6121d
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-25 12:16:23 +0530

    OtpController.java Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/controller/OtpController.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/controller/OtpController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/auth")
 public class OtpController {
 
     private final OTPService otpService;
