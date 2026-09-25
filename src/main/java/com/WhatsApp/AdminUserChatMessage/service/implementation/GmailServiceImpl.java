2a16f47bf0811a9d6441c9b2f492edd33c982557
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-25 14:49:00 +0530

    GmailServiceImpl.java Added and Modified

--- src/main/java/com/WhatsApp/AdminUserChatMessage/service/implementation/GmailServiceImpl.java
+++ src/main/java/com/WhatsApp/AdminUserChatMessage/service/implementation/GmailServiceImpl.java
@@ -1,4 +1,26 @@
 package com.WhatsApp.AdminUserChatMessage.service.implementation;
 
-public class GmailServiceImpl {
+import com.WhatsApp.AdminUserChatMessage.service.GmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GmailServiceImpl implements GmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    @Override
+    public void sendEmailOtp(String email, String otp) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("WhatsApp Email Verification Code");
+        message.setText("Welcome to WhatsApp Email Verification Code.\n"
+                +"Your OTP code for Email Verification code is " + otp + "\n"
+                +"Thank You"
+        );
+        javaMailSender.send(message);
+    }
 }
